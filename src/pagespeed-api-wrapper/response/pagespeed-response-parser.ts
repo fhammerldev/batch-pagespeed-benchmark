b98567ef1b14f5fcb4d43a745dbae30148948b88
Ferenc Hammerl <fhammerl.dev@gmail.com>
2019-04-23 15:47:23 +0200

    Fix tslint error (prevent prototype key leaks)

--- src/pagespeed-api-wrapper/response/pagespeed-response-parser.ts
+++ src/pagespeed-api-wrapper/response/pagespeed-response-parser.ts
@@ -5,9 +5,12 @@ export class PagespeedResponseParser {
     public parse(dump: any): PageSpeedResult {
         const audits: Audit[] = [];
         const rawAudits: any = dump.lighthouseResult.audits;
-        for (const auditId in rawAudits) {
-            audits.push(rawAudits[auditId] as Audit);
+        for (const key in rawAudits) {
+            if (rawAudits.hasOwnProperty(key)) {
+                audits.push(rawAudits[key] as Audit);
+            }
         }
+
         return new PageSpeedResult(dump.id, audits, dump.lighthouseResult.categories.performance.score);
     }
 }
